9f764befbfc19e504bcb86e0385bf2203c27c527
tuui <them62@hotmail.com>
2017-06-07 23:57:27 +0300

    react history first implementation

--- web/react/src/History.js
+++ web/react/src/History.js
@@ -1,25 +1,45 @@
 import React from 'react';
+import BetTable from './BetTable';
 
 class History extends React.Component {
+
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	      bets: [],
+	    }
+	  }
+
+	componentDidMount() {
+	    console.log('componentDidMount');
+
+
+	return fetch('/api/search', {
+	  method: 'POST',
+	  headers: {
+	    'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify({
+	    pageNr: 1,
+	    pageSize: 10,
+	  })
+	})
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          bets: responseJson.bets,
+        }, function() {
+          console.log('new state do something');
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
-		<table className="table table-hover">
-		    <thead>
-		    <tr>
-		        <th>id</th>
-		        <th>eventId</th>
-		        <th>betOfferId</th>
-		        <th>username</th>
-		        <th>stake</th>
-		        <th>odds</th>
-		        <th>status</th>
-		        <th>placed</th>
-		    </tr>
-		    </thead>
-		    <tbody>
-
-		    </tbody>
-		</table>
+		<BetTable bets={this.state.bets}/>
     );
   }
 }
